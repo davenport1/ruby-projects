a996c4b8609c786bffb1f06c0315d42cfe82f35c
Matthew Davenport <davenmport@gmail.com>
2022-06-27 12:36:17 -0700

    Game functionality working. Still not detecting a winner

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,140 +1,156 @@
 # Tic Tac Toe CLI game in ruby
 
-class tic_tac_toe
+class TicTacToe
 
+	#turn will cycle through each players turn and check after each turn
+	#if a player has won a game before reiterating another round
 	def turn
 		play = true 
-		if intro 
-			while play do 
+			while play
 				input_player_1
-				if check_win 
+				if check_win?(1)
 					player_win(1)
 					play = false
 				end
 				if play 
 					input_player_2
-					if check_win
+					if check_win?(2)
 						player_win(2)
 						play = false
 					end
 				end
 				if play && board_full
-					tie_game
+					puts "Board has been filled.. tie game!"
 					play = false
 				end
+			
 			end
-			end
-		end
-		
-		else
-			puts "Thanks for playing! Goodbye."
 	end
 
-	def display_board(board)
-
+	#displays the 3x3 tic tac toe board with all the current moves
+	def display_board
+		puts "Current board is: "
+		puts " #{@@board[0][0]} | #{@@board[0][1]} | #{@@board[0][2]}" 
+		puts " #{@@board[1][0]} | #{@@board[1][1]} | #{@@board[1][2]}" 
+		puts " #{@@board[2][0]} | #{@@board[2][1]} | #{@@board[2][2]}" 
 	end
 
-	def intro
-		puts "Welcome to tic tac toe! Press 1 to play"
-		result = gets.chomp
-		if(result == 1) return true
-		else return false 
-		end
-	end
+	#asks user if they would like to play tic tac toe
+	# def intro
+	# 	puts "Welcome to tic tac toe! Press 1 to play"
+	# 	result = gets.chomp
+	# 	if result == 1 return true
+	# 	else return false 
+	# 	end
+	# end
 
-	def check_win(player)
+	#will check if any of the 8 winning combinations is
+	#currently on the board
+	#if so will announce the winner as the current plyer that was
+	#passed in
+	def check_win?(player)
 		win = false
-		case board
-		when board[1][1] == board[1][2] && board[1][1] == board[1][3]
+		case @@board
+		when @@board[0][0] == @@board[0][1] && @@board[0][0] == @@board[0][2]
 			win = true
-			break 
-		when board[2][1] == board[2][2] && board[2][1] == board[2][3]
+		when @@board[1][0] == @@board[1][1] && @@board[1][0] == @@board[1][2]
 			win = true
-			break
-		when board[3][1] == board[3][2] && board[3][1] == board[3][3]
+		when @@board[2][0] == @@board[2][1] && @@board[2][0] == @@board[2][2]
 			win = true
-			break
-		when board[1][1] == board[2][1] && board[1][1] == board[3][1]
+		when @@board[0][0] == @@board[1][0] && @@board[0][0] == @@board[2][0]
 			win = true
-			break
-		when board[1][2] == board[2][2] && board[1][2] == board[3][2]
+		when @@board[0][1] == @@board[1][1] && @@board[0][1] == @@board[2][1]
 			win = true
-			break
-		when board[1][3] == board[2][3] && board[1][3] == board[3][3]
+		when @@board[0][2] == @@board[1][2] && @@board[0][2] == @@board[2][2]
+			win = true 
+		when @@board[0][0] == @@board[1][1] && @@board[0][0] == @@board[2][2]
 			win = true
-			break
-		when board[1][1] == board[2][2] && board[1][1] == board[3][3]
-			win = true
-			break
-		when board[1][3] == board[1][2] && board[1][3] == board[3][1]
+		when @@board[0][2] == @@board[1][1] && @@board[0][2] == @@board[2][0]
 			win = true
 		end
 
 		if win 
 			player_win(player)
 		end
+
+		return win
 		
 	end
 
+	#Will determine if the current move requested by the player is valid
 	def valid_move?(row, column)
-		if(board[row][column] == '')
+		if(@@board[row][column] == ' ')
 			return true
 		else
 			return false
 		end
 	end
 
+	#Takes input from the current player
 	def input_player_1
+		display_board
 		valid_move = false
-		while valid_move do 
+		while !valid_move 
 			puts "Player 1: input the row you would like to move"
-			row = gets.chomp
+			row = gets.chomp.to_i - 1
 			puts "Player 1: input the column you would like to move"
-			column = gets.chomp
+			column = gets.chomp.to_i - 1
 			valid_move = valid_move?(row, column)
 			if !valid_move 
 				puts "Please enter a valid square that doesn't already contain a move"
 			end
 		end
-		end
-		board[row][column] = 'X'
+		@@board[row][column] = 'X'
 
 	end
 
 	def input_player_2
+		display_board
 		valid_move = false
-		while valid_move do 
+		while !valid_move
 			puts "Player 2: input the row you would like to move"
-			row = gets.chomp
+			row = gets.chomp.to_i - 1
 			puts "Player 2: input the column you would like to move"
-			column = gets.chomp
+			column = gets.chomp.to_i - 1	
 			valid_move = valid_move?(row, column)
 			if !valid_move 
 				puts "Please enter a valid square that doesn't already contain a move"
 			end
 		end
-		end
-		board[row][column] = 'O'
+		
+		@@board[row][column] = 'O'
 	end
 
+	#announces current player as winner
 	def player_win(player)
 		puts "Player #{player} wins!"
 	end
 
-	def tie_game
+	#board has been filled and therefore is a tied game
+	def board_full
+		empty_board = true
+		@@board.each do |row| 
+			row.each do |space|
+				if space == ' '
+					empty_board = false
+				end
+			end
+		end
+		return empty_board
 
 	end
 
 
-
-	private
-
-	board = [
-				['','','']
-				['','','']
-				['','','']
+	@@board = [
+			[' ',' ',' '],
+			[' ',' ',' '],
+			[' ',' ',' ']
 			]
 
-	turn_count = 0
-
+	@@turn_count = 0
 end
+
+
+
+current_board = TicTacToe.new()
+current_board.turn
